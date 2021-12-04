012325891780b9053cab248ce512f34be4f5cf13
Nishan Goswami <nishangoswami@gmail.com>
2021-12-04 16:59:56 +0530

    First Commit For Project

---
+++ src/pages/speeddial.js
@@ -0,0 +1,15 @@
+
+
+function SpeedDial() {
+    return (
+        <div>
+            <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)' }}>
+                <Item>1</Item>
+                <Item>2</Item>
+                <Item>3</Item>
+            </Box>
+        </div>
+    );
+}
+
+export default SpeedDial;
